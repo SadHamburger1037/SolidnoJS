cf927f106cb9d903268e13335a496582301c2de2
SadHamburger1037 <marioheadphones6@gmail.com>
2024-09-16 10:45:17 +0200

    SolidnoJS

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import styles from './App.module.css';
+import MyComponent from './MyComponent';
+
+function App() {
+  return (
+    <>
+      <div class={styles.App}>
+        Ide gas Baka Prase
+      </div>
+      <MyComponent> </MyComponent>
+    </>
+  );
+}
+
+export default App;
---
+++ src/MyComponent.jsx
@@ -0,0 +1,9 @@
+import styles from "./MyComponent.module.css";
+
+export default function MyComponent() {
+    return (
+        <>
+            <div class={styles.kutija}>Moja druga komponenta</div>
+        </>
+    );
+}
